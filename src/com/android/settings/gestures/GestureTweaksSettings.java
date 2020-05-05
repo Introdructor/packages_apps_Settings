ezio84/abc-n_packages_apps_Settings

Allow a wider back swipe to trigger actions (instead of timeout) [2/2]

src/com/android/settings/gestures/GestureTweaksSettings.java
@@ -38,6 +38,8 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
 
 import com.abc.support.preferences.CustomSeekBarPreference;
+import com.abc.support.preferences.SystemSettingListPreference;
+import com.abc.support.preferences.SystemSettingSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +54,8 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
     private ListPreference mRightSwipeActions;
     private Preference mLeftSwipeAppSelection;
     private Preference mRightSwipeAppSelection;
+    private SystemSettingListPreference mTimeout;
+    private SystemSettingSwitchPreference mExtendedSwipe;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -86,6 +90,15 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
                 Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
         mRightSwipeAppSelection.setEnabled(isAppSelection);
         customAppCheck();
+
+        mTimeout = (SystemSettingListPreference) findPreference("long_back_swipe_timeout");
+        mExtendedSwipe = (SystemSettingSwitchPreference) findPreference("back_swipe_extended");
+        boolean extendedSwipe = Settings.System.getIntForUser(resolver,
+            Settings.System.BACK_SWIPE_EXTENDED, 0,
+            UserHandle.USER_CURRENT) != 0;
+        mExtendedSwipe.setChecked(extendedSwipe);
+        mExtendedSwipe.setOnPreferenceChangeListener(this);
+        mTimeout.setEnabled(!mExtendedSwipe.isChecked());
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -111,6 +124,10 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
             mRightSwipeAppSelection.setEnabled(rightSwipeActions == 5);
             customAppCheck();
             return true;
+        } else if (preference == mExtendedSwipe) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            mExtendedSwipe.setChecked(enabled);
+            mTimeout.setEnabled(!enabled);
         }
         return false;
     }

Introdructor/packages_apps_Settings

Change-Id: Ic23aa2824b41b7bc2cd9b9e2d25dd0f01d99cccc
Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

src/com/android/settings/gestures/GestureTweaksSettings.java
@@ -22,22 +22,19 @@ import android.os.Bundle;
 import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import androidx.preference.PreferenceCategory;
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
-import androidx.preference.PreferenceScreen;
 import androidx.preference.Preference.OnPreferenceChangeListener;
-import androidx.preference.SwitchPreference;
-
 import com.android.internal.logging.nano.MetricsProto;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
 
 import com.rebellion.support.preferences.CustomSeekBarPreference;
+import com.rebellion.support.preferences.SystemSettingListPreference;
+import com.rebellion.support.preferences.SystemSettingSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +49,8 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
     private ListPreference mRightSwipeActions;
     private Preference mLeftSwipeAppSelection;
     private Preference mRightSwipeAppSelection;
+    private SystemSettingListPreference mTimeout;
+    private SystemSettingSwitchPreference mExtendedSwipe;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -86,6 +85,15 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
                 Settings.System.RIGHT_LONG_BACK_SWIPE_ACTION, 0, UserHandle.USER_CURRENT) == 5/*action_app_action*/;
         mRightSwipeAppSelection.setEnabled(isAppSelection);
         customAppCheck();
+
+        mTimeout = (SystemSettingListPreference) findPreference("long_back_swipe_timeout");
+        mExtendedSwipe = (SystemSettingSwitchPreference) findPreference("back_swipe_type");
+        boolean extendedSwipe = Settings.System.getIntForUser(resolver,
+            Settings.System.BACK_SWIPE_TYPE, 0,
+            UserHandle.USER_CURRENT) != 0;
+        mExtendedSwipe.setChecked(extendedSwipe);
+        mExtendedSwipe.setOnPreferenceChangeListener(this);
+        mTimeout.setEnabled(!mExtendedSwipe.isChecked());
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -111,6 +119,10 @@ public class GestureTweaksSettings extends SettingsPreferenceFragment
             mRightSwipeAppSelection.setEnabled(rightSwipeActions == 5);
             customAppCheck();
             return true;
+        } else if (preference == mExtendedSwipe) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            mExtendedSwipe.setChecked(enabled);
+            mTimeout.setEnabled(!enabled);
         }
         return false;
     }